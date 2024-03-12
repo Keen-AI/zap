5bb0b5fd4ca7c1c44db0e8b6495c32187edc14c4
Petar Gyurov <petar.gyurov@keen-ai.com>
2024-03-12 16:16:34 +0000

    Enriched test method with mAP calc

--- src/zap/object_detection/models.py
+++ src/zap/object_detection/models.py
@@ -26,7 +26,10 @@ SOFTWARE.
 
 import lightning.pytorch as pl
 import torch
-from transformers import DetaForObjectDetection
+from torch import tensor
+from torchmetrics.detection import MeanAveragePrecision
+from transformers import DetaForObjectDetection, DetaImageProcessor
+from transformers.image_transforms import center_to_corners_format
 
 
 class Deta(pl.LightningModule):
@@ -35,15 +38,23 @@ class Deta(pl.LightningModule):
         # replace COCO classification head with custom head
         # we specify the "no_timm" variant here to not rely on the timm library
         # for the convolutional backbone
-        self.model = DetaForObjectDetection.from_pretrained("jozhang97/deta-resnet-50",
+        self.pretrained_model = "jozhang97/deta-resnet-50"  # TODO: make this config driven
+        self.model = DetaForObjectDetection.from_pretrained(self.pretrained_model,
                                                             num_labels=num_classes,
                                                             auxiliary_loss=True,
                                                             ignore_mismatched_sizes=True)
-        # see https://github.com/PyTorchLightning/pytorch-lightning/pull/1896
+
+        self.processor = DetaImageProcessor.from_pretrained(self.pretrained_model,
+                                                            size={"shortest_edge": 432,  # TODO: needs to match config
+                                                                  "longest_edge": 768})
+
         self.lr = lr
         self.lr_backbone = lr_backbone
         self.weight_decay = weight_decay
         self.save_hyperparameters()
+
+        # metrics
+        self.precision = MeanAveragePrecision(class_metrics=False)
 
     def forward(self, pixel_values, pixel_mask=None):
         outputs = self.model(pixel_values=pixel_values, pixel_mask=pixel_mask)
@@ -54,6 +65,7 @@ class Deta(pl.LightningModule):
         pixel_values = batch["pixel_values"]
         pixel_mask = batch["pixel_mask"]
         labels = [{k: v.to(self.device) for k, v in t.items()} for t in batch["labels"]]
+
         try:
             outputs = self.model(pixel_values=pixel_values, pixel_mask=pixel_mask, labels=labels)
         except AssertionError:  # TODO: handle this properly
@@ -64,19 +76,17 @@ class Deta(pl.LightningModule):
         loss = outputs.loss
         loss_dict = outputs.loss_dict
 
-        return loss, loss_dict
+        return loss, loss_dict, labels, outputs
 
     def training_step(self, batch, batch_idx):
-        loss, loss_dict = self.common_step(batch, batch_idx)
-        # logs metrics for each training_step,
-        # and the average across the epoch
+        loss, loss_dict, _, _ = self.common_step(batch, batch_idx)
         self.log("train_loss", loss)
         for k, v in loss_dict.items():
             self.log("train_" + k, v.item())
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss, loss_dict = self.common_step(batch, batch_idx)
+        loss, loss_dict, _, _ = self.common_step(batch, batch_idx)
         self.log("val_loss", loss)
         for k, v in loss_dict.items():
             self.log("validation_" + k, v.item())
@@ -84,9 +94,36 @@ class Deta(pl.LightningModule):
         return loss
 
     def test_step(self, batch, batch_idx):
-        loss, loss_dict = self.common_step(batch, batch_idx)
-        # logs metrics for each training_step,
-        # and the average across the epoch
+        loss, loss_dict, labels, outputs = self.common_step(batch, batch_idx)
+
+        H, W = labels[0]['orig_size'].tolist()
+        labels[0]['labels'] = labels[0].pop('class_labels')
+
+        # format GT bboxes to same format as results (xyxy) and un-normalise
+        xyxy_gt_boxes = center_to_corners_format(labels[0]['boxes']).tolist()
+        xyxy_scaled_gt_boxes = []
+        for b in xyxy_gt_boxes:
+            xmin, ymin, xmax, ymax = b
+
+            xmin = xmin * W
+            ymin = ymin * H
+            xmax = xmax * W
+            ymax = ymax * H
+
+            xyxy_scaled_gt_boxes.append([xmin, ymin, xmax, ymax])
+
+        labels[0]['boxes'] = tensor(xyxy_scaled_gt_boxes)
+
+        #  get results from model
+        results = self.processor.post_process_object_detection(outputs,
+                                                               target_sizes=[(H, W)],
+                                                               threshold=0)
+
+        # calc and log precision
+        precision = self.precision(results, labels)
+        for k, v in precision.items():
+            self.log(k, v.item(), on_epoch=True)
+
         self.log("test_loss", loss)
         for k, v in loss_dict.items():
             self.log("test_" + k, v.item())
