d9e63f27dbfde8c834f8ba47de4e055af48d9d0a
Petar Gyurov <petar.gyurov@keen-ai.com>
2024-08-13 14:31:00 +0100

    Refactored dataset generator to include a simple way to generate classification suitable images and labels

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,52 +1,57 @@
+import csv
 import json
 import math
 import random
 
 from PIL import Image, ImageDraw
 
+MODE = 'classification'  # or coco
+NUMBER_OF_IMAGES = 100
+IMAGE_SIZE = 244
+SHAPE_SIZE = IMAGE_SIZE // 10
+SAVE_DIR = 'data/gen_data'
 
-def generate_shape(draw, quadrant):
+
+def _generate_shape(draw, quadrant):
     color = random.choice(["red", "green", "blue"])
     shape_type = random.choice(["circle", "square", "triangle"])
 
-    size = 8
-
     if quadrant == 1:
-        x = random.randint(size, 31 - size)
-        y = random.randint(size, 31 - size)
+        x = random.randint(SHAPE_SIZE, IMAGE_SIZE // 2 - SHAPE_SIZE)
+        y = random.randint(SHAPE_SIZE, IMAGE_SIZE // 2 - SHAPE_SIZE)
     elif quadrant == 2:
-        x = random.randint(32 + size, 63 - size)
-        y = random.randint(size, 31 - size)
+        x = random.randint(IMAGE_SIZE // 2 + SHAPE_SIZE, IMAGE_SIZE - SHAPE_SIZE)
+        y = random.randint(SHAPE_SIZE, IMAGE_SIZE // 2 - SHAPE_SIZE)
     elif quadrant == 3:
-        x = random.randint(size, 31 - size)
-        y = random.randint(32 + size, 63 - size)
+        x = random.randint(SHAPE_SIZE, IMAGE_SIZE // 2 - SHAPE_SIZE)
+        y = random.randint(IMAGE_SIZE // 2 + SHAPE_SIZE, IMAGE_SIZE - SHAPE_SIZE)
     elif quadrant == 4:
-        x = random.randint(32 + size, 63 - size)
-        y = random.randint(32 + size, 63 - size)
+        x = random.randint(IMAGE_SIZE // 2 + SHAPE_SIZE, IMAGE_SIZE - SHAPE_SIZE)
+        y = random.randint(IMAGE_SIZE // 2 + SHAPE_SIZE, IMAGE_SIZE - SHAPE_SIZE)
 
     if shape_type == "circle":
-        draw.ellipse((x - size, y - size, x + size, y + size), fill=color)
+        draw.ellipse((x - SHAPE_SIZE, y - SHAPE_SIZE, x + SHAPE_SIZE, y + SHAPE_SIZE), fill=color)
     elif shape_type == "square":
-        draw.rectangle((x - size, y - size, x + size, y + size), fill=color)
+        draw.rectangle((x - SHAPE_SIZE, y - SHAPE_SIZE, x + SHAPE_SIZE, y + SHAPE_SIZE), fill=color)
     elif shape_type == "triangle":
-        points = [(x, y - size), (x - size, y + size), (x + size, y + size)]
+        points = [(x, y - SHAPE_SIZE), (x - SHAPE_SIZE, y + SHAPE_SIZE), (x + SHAPE_SIZE, y + SHAPE_SIZE)]
         draw.polygon(points, fill=color)
 
     shape_info = {
-        "category_id": ["circle", "square", "triangle"].index(shape_type) + 1,
-        "bbox": [x - size, y - size, 2 * size, 2 * size],
-        "segmentation": get_segmentation(shape_type, x, y, size),
+        "category_id": ["circle", "square", "triangle"].index(shape_type),
+        "bbox": [x - SHAPE_SIZE, y - SHAPE_SIZE, 2 * SHAPE_SIZE, 2 * SHAPE_SIZE],
+        "segmentation": _get_segmentation(shape_type, x, y, SHAPE_SIZE),
     }
 
-    return shape_info
+    return shape_info, shape_type
 
 
-def get_segmentation(shape_type, x, y, size, P=30):
+def _get_segmentation(shape_type, x, y, SHAPE_SIZE, P=30):
 
     if shape_type == "circle":
         theta = [2 * math.pi * i / P for i in range(P)]
         circle_points = [
-            (x + (size + 1) * math.cos(angle), y + (size + 1) * math.sin(angle))
+            (x + (SHAPE_SIZE + 1) * math.cos(angle), y + (SHAPE_SIZE + 1) * math.sin(angle))
             for angle in theta
         ]
 
@@ -55,30 +60,30 @@ def get_segmentation(shape_type, x, y, size, P=30):
 
     elif shape_type == "square":
         return [
-            x - size - 1,
-            y - size - 1,
-            x + size + 2,
-            y - size - 1,
-            x + size + 2,
-            y + size + 2,
-            x - size - 1,
-            y + size + 2,
+            x - SHAPE_SIZE - 1,
+            y - SHAPE_SIZE - 1,
+            x + SHAPE_SIZE + 2,
+            y - SHAPE_SIZE - 1,
+            x + SHAPE_SIZE + 2,
+            y + SHAPE_SIZE + 2,
+            x - SHAPE_SIZE - 1,
+            y + SHAPE_SIZE + 2,
         ]
 
     elif shape_type == "triangle":
         return [
             x + 1,
-            y - size - 2,
+            y - SHAPE_SIZE - 2,
 
-            x - size - 1,
-            y + size + 2,
+            x - SHAPE_SIZE - 1,
+            y + SHAPE_SIZE + 2,
 
-            x + size + 2,
-            y + size + 2
+            x + SHAPE_SIZE + 2,
+            y + SHAPE_SIZE + 2
         ]
 
 
-def generate_coco_labels(image_id, width, height, shapes, current_annots):
+def _generate_coco_labels(image_id, width, height, shapes, current_annots):
     annotations = []
 
     for idx, shape in enumerate(shapes):
@@ -95,7 +100,7 @@ def generate_coco_labels(image_id, width, height, shapes, current_annots):
         annotations.append(annotation)
 
     coco_labels = {
-        "categories": [{"id": 1, "name": "circle"}, {"id": 2, "name": "square"}, {"id": 3, "name": "triangle"}],
+        "categories": [{"id": 0, "name": "circle"}, {"id": 1, "name": "square"}, {"id": 2, "name": "triangle"}],
         "annotations": annotations,
         "info": [],
     }
@@ -103,7 +108,7 @@ def generate_coco_labels(image_id, width, height, shapes, current_annots):
     return coco_labels
 
 
-def main(N):
+def generate_coco_data():
     coco_data = {
         'images': [],
         'categories': [],
@@ -111,34 +116,56 @@ def main(N):
         'info': []
     }
 
-    for i in range(N):
-        img_name = f'data/gen_data/generated_image_{i}.png'
-        img = Image.new("RGB", (64, 64), color="white")
+    for i in range(NUMBER_OF_IMAGES):
+        img_name = f'generated_image_{i}.png'
+        img = Image.new("RGB", (IMAGE_SIZE, IMAGE_SIZE), color="white")
         draw = ImageDraw.Draw(img)
 
         quadrants = [1, 2, 3, 4]
         shapes = []
         for q in quadrants:
-            shapes.append(generate_shape(draw, q))
+            shapes.append(_generate_shape(draw, q)[0])
 
-        coco_labels = generate_coco_labels(i, 64, 64, shapes, len(coco_data['annotations']))
+        coco_labels = _generate_coco_labels(i, IMAGE_SIZE, IMAGE_SIZE, shapes, len(coco_data['annotations']))
 
         coco_data['images'].append({
-            'width': 64,
-            'height': 64,
+            'width': IMAGE_SIZE,
+            'height': IMAGE_SIZE,
             'id': i,
             'file_name': img_name
         })
         coco_data['categories'] = coco_labels['categories']
         coco_data['annotations'] += coco_labels['annotations']
 
-        img.save(img_name)
+        img.save(f'{SAVE_DIR}images/{img_name}')
 
     # Save COCO labels in a JSON file
-    with open("data/gen_data/coco_labels.json", "w") as json_file:
+    with open(f"{SAVE_DIR}labels.json", "w") as json_file:
         json.dump(coco_data, json_file)
 
 
+def generate_classification_data():
+    classifier_data = []
+    for i in range(NUMBER_OF_IMAGES):
+        img_name = f'generated_image_{i}.png'
+        img = Image.new("RGB", (IMAGE_SIZE, IMAGE_SIZE), color="white")
+        draw = ImageDraw.Draw(img)
+
+        _, shape = _generate_shape(draw, 1)
+
+        classifier_data.append({'filename': img_name, 'label': shape})
+
+        img.save(f'{SAVE_DIR}/images/{img_name}')
+
+    with open(f"{SAVE_DIR}/labels.csv", "w", newline='') as csv_file:
+        column_names = ['filename', 'label']
+        csv_writer = csv.DictWriter(csv_file, fieldnames=column_names)
+        csv_writer.writeheader()
+        csv_writer.writerows(classifier_data)
+
+
 if __name__ == "__main__":
-    N = 100  # Change N to the desired number of images
-    main(N)
+    if MODE == 'classification':
+        generate_classification_data()
+    elif MODE == 'coco':
+        generate_coco_data()
